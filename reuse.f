70854944975a5ecb7b9173d15e1a4af747f2e77d
Foez Ahmed <foez.official@gmail.com>
2026-08-06 14:16:56 +0600

    auto-update

--- reuse.f
+++ reuse.f
@@ -1,5 +1,6 @@
 ${ADN_ENDEC}/interface/uart_if.sv
 ${ADN_ENDEC}/source/adn_uart_receiver.sv
+${ADN_ENDEC}/source/adn_uart_transmitter.sv
 ${ADN_ENDEC}/source/dummy_rtl.sv
 -i ${ADN_COMMON}/include
 -i ${ADN_ENDEC}/include
