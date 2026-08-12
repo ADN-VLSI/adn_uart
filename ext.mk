70971d287d64b1d58c75ac585e965b3577298f71
Foez Ahmed <foez.official@gmail.com>
2026-08-12 16:08:28 +0600

    modified:   ext.mk

--- ext.mk
+++ ext.mk
@@ -1,7 +1,7 @@
 HAS_SUBMODULES = 1
 
-export ADN_ENDEC=$(CURDIR)
-export REPO_NAME_EXP=ADN_ENDEC
+export ADN_UART=$(CURDIR)
+export REPO_NAME_EXP=ADN_UART
 
 export ADN_COMMON=$(REPO_ROOT)/submodule/adn_common
 
