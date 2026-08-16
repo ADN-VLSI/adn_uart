08c286a9ca1dc90a70d1a7a95f01f57b3762ac8d
Ahasan Ullah Khalid <aukhalid02@gmail.com>
2026-08-16 15:48:08 +0600

    modified:   source/adn_uart_register_interface.sv

--- source/adn_uart_register_interface.sv
+++ source/adn_uart_register_interface.sv
@@ -1,8 +1,8 @@
 /*
 
-@foez---bhai, write the purpose of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
+@foez-bhai, write the purpose of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
 
-@foez---bhai, describe the use case of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
+@foez-bhai, describe the use case of this module in markdown format here. This is already in multi-line comment, so don't add any additional comment syntax.
 
 | REVISION | DATE       | AUTHOR              | DESCRIPTION                                            |
 |----------|------------|---------------------|--------------------------------------------------------|
@@ -17,7 +17,7 @@ See LICENSE file in the project root for full license information
 
 */
 
-// @foez---bhai, add comments to the parameters, ports
+// @foez-bhai, add comments to the parameters, ports
 module adn_uart_register_interface #(
     parameter int ADDR_WIDTH = 32,
     parameter int DATA_WIDTH = 32,
